9a98b904fcd43e3b99d0cd5e3492c2bc9d0eec5f
Dean Lark <48802382+dtlark@users.noreply.github.com>
2022-06-12 15:37:06 -0400

    removed matcher

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,10 @@ use std::io::Read;
 use std::collections::VecDeque;
 use std::collections::HashMap;
 
-
 #[derive(Serialize, Deserialize)]
-struct Order {
+pub struct Order {
     otype: char,
+    order_id: u32,
     firm_id: u32,
     symbol: String,
     quantity: u32,
@@ -18,34 +18,23 @@ struct Order {
     price: f32
 }
 
-struct Matcher {
-    buys: VecDeque<u32>,
-    sells: VecDeque<u32>,
+pub struct Stock {
+    buys: VecDeque<(u32, f32)>,
+    sells: VecDeque<(u32, f32)>,
     firms: HashMap<u32, bool>,
 }
 
-impl Default for Matcher {
-    fn default() -> Matcher {
-        Matcher {
-            buys: VecDeque::new(),
-            sells: VecDeque::new(),
-            firms: HashMap::new()
-        }
+pub fn new_order(order: &Order, stock_orders: &HashMap<String, Stock>) { //pub fn new_order(firmId: u32, symbol: &String, side: bool, price: f32) 
+    if !stock_orders.contains_key(&order.symbol) {
+        println!("In new order");
     }
 }
 
-impl Matcher {
-    pub fn new_order(firmId: u32, symbol: &String, side: bool, price: f32) {
-
-    }
-    pub fn modify_order(firmId: u32, symbol: &String, price: f32) {
-
-    }
-    pub fn cancel_order(firmId: u32, symbol: &String) {
-
-    }
+pub fn modify_order(firm_id: u32, symbol: &String, price: f32) {
 }
 
+pub fn cancel_order(firm_id: u32, symbol: &String) {
+}
 
 fn main() {
 
@@ -55,11 +44,14 @@ fn main() {
 
     let orders: Vec<Order> = serde_json::from_str(&data).unwrap();
 
+    let stock_orders: HashMap<String, Stock> = HashMap::new();
+    let firm_info: HashMap<String, (u32, u32, u32)> = HashMap::new();
+
     for order in orders.iter() {
         match order.otype {
-            'N'=>Matcher::new_order(order.firm_id, &order.symbol, order.side, order.price),
-            'M'=>Matcher::modify_order(order.firm_id, &order.symbol, order.price),
-            'C'=>Matcher::cancel_order(order.firm_id, &order.symbol),
+            'N'=>new_order(order, &stock_orders),
+            'M'=>modify_order(order.firm_id, &order.symbol, order.price),
+            'C'=>cancel_order(order.firm_id, &order.symbol),
             _=>println!("Invalid order type"),
         }
         println!("{:#?}", order.otype);
